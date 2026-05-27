4ea5b9603eaf0e39a2ae25c1131f4e36d4ee5b1a
dawtom97 <dawtom97@gmail.com>
2026-05-27 19:42:07 +0200

    pndas implementation

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ load_dotenv()
 class Config:
     API_KEY = os.getenv("API_KEY")
     API_QUERY = os.getenv("API_QUERY")
+    EXCEL_PATH = "weather.xlsx"
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from services.openweather_api import get_weather
+from services.excel_files import save_to_excel
 import time
 
 while True:
     weather = get_weather()
-    print(weather)
+    save_to_excel([weather])
     print("Udało się pobrać dane")
     time.sleep(10)
 
