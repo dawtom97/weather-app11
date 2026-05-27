6c6dbed532b677a42330f05ce0bcdf1b24e21f8e
dawtom97 <dawtom97@gmail.com>
2026-05-27 18:03:55 +0200

    bugfix 27.05

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from services.openweather_api import get_weather
 import time
 
-
 while True:
     weather = get_weather()
     print(weather)
+    print("Udało się pobrać dane")
     time.sleep(10)
 
-
-# git commit -m "initial commit"
-
-new_var = 123
 
 
 
